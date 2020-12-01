3a7f6bbb2f75126c8e3228d6387cab294d790203
Grant Spencer <gwspence22@gmail.com>
2020-11-30 20:58:17 -0500

    first commit

---
+++ ch2_project1_student_registration.Project/ch2_project1_student_registration.Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ch2_project1_student_registration.Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
